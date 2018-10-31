29e67da186aa14c25b20c918b280cb8ac251fbee
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2018-10-31 16:46:39 +0800

    initialState.js

---
+++ my-app/src/redux/store/initialState.js
@@ -0,0 +1,14 @@
+/**
+ * 本文件的作用就是直观  整个应用状态结构树  及其  初始值
+ */
+
+ const initialState = {
+    adminUserInfo: {
+      phone: '15888888888',
+      realName: 'caiyu'
+    },
+    user: 'test',
+    test: 'redux-test'
+ }
+
+ export default initialState
