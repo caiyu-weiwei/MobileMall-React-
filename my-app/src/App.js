a5523ea29ec556a5837f89936523f2fc6b575995
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2018-11-02 17:08:19 +0800

    change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './App.css'
 import logo from '@/assets/imgs/logo.png'
 import '@/assets/stylus/reset.styl'
+import { Tabs, WhiteSpace  } from 'antd-mobile'
+import { StickyContainer, Sticky } from 'react-sticky'
 
 class App extends Component {
 
@@ -12,6 +14,12 @@ class App extends Component {
           <img className="App-logo" src={logo} alt="logo"/>
           <h1 className="App-title">Mango Music</h1>
         </header>
+        <div className="music-tab">
+          <WhiteSpace />
+          <StickyContainer>
+            
+          </StickyContainer>
+        </div>
       </div>
     )
   }
