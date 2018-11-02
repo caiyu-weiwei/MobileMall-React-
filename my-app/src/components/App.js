32bb13ba7b3b7724ab3d29cad9b61f01b6d6129f
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2018-11-02 16:46:12 +0800

    App.js

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import './App.css'
+import logo from '@/assets/imgs/logo.png'
+import '@/assets/stylus/reset.styl'
 
 class App extends Component {
 
   render() {
     return (
       <div className= "App">
-        Welcome to React
+        <header className="App-header">
+          <img className="App-logo" src={logo} alt="logo"/>
+          <h1 className="App-title">Mango Music</h1>
+        </header>
       </div>
     )
   }
