2e4fe9b246c0a8d9c887801ba4ebb1a0c8dd30d5
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2018-11-06 15:15:40 +0800

    api config

---
+++ my-app/src/api/config.js
@@ -0,0 +1,8 @@
+const URL = {
+
+  /*推荐轮播*/
+  carousel: "https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg",
+  
+  /*最新专辑*/
+  newalbum: "https://u.y.qq.com/cgi-bin/musicu.fcg"
+}
