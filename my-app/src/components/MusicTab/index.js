61ce0169f5e1e41f6b0c2f64dbccb0949cc1b6d1
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2018-11-06 15:12:35 +0800

    music-tabs

--- my-app/src/components/MusicTab/index.js
+++ my-app/src/components/MusicTab/index.js
@@ -5,6 +5,7 @@ import Recommend from '@/components/Recommend'
 import Rinking from '@/components/Rinking'
 import Search from '@/components/Search'
 import './index.css'
+import { BrowserRouter as Router, Route, Switch, Redirect, NavLink } from 'react-router-dom'
 
 class MusicTab extends Component {
 
@@ -24,21 +25,34 @@ class MusicTab extends Component {
 
   render () {
     return (
-      <div className="music-tab">
-        <WhiteSpace />
-        <StickyContainer>
-          <Tabs
-            tabs={this.tabs}
-            tabBarUnderlineStyle={{borderBottom:'1px solid #FFD500'}}
-            tabBarBackgroundColor="#212121"
-            tabBarActiveTextColor="#FFD700"  
-          >
-            <Recommend></Recommend>
-            <Rinking></Rinking>
-            <Search></Search>
-          </Tabs>
-        </StickyContainer>
-      </div>
+      <Router>
+        <div className="music-tab">
+          <WhiteSpace />
+          <StickyContainer>
+
+            <div className="music-tabs">
+              <div className="tabs-item">
+                <NavLink to="/recommend" className="nav-link">推荐</NavLink>
+              </div>
+              <div className="tabs-item">
+                <NavLink to="/rinking" className="nav-link">排行榜</NavLink>
+              </div>
+              <div className="tabs-item">
+                <NavLink to="/search" className="nav-link">搜索</NavLink>
+              </div>
+            </div>
+          </StickyContainer>
+          <div>
+            <Switch>
+              <Route path="/recommend" component={Recommend}></Route>
+              <Route path="/rinking" component={Rinking}></Route>
+              <Route path="/search" component={Search}></Route>
+              <Redirect from="/" to="/recommend"></Redirect>
+              <Route component={Recommend}></Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
     )
   }
 }
