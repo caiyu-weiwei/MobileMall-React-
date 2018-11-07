915261126c7cafff31d8905cfaec2d7cc9af23a7
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2018-11-07 09:59:08 +0800

    recommend.js

--- my-app/src/api/recommend.js
+++ my-app/src/api/recommend.js
@@ -12,3 +12,21 @@ export async function getCarousel() {
 
   return await jsonp(URL.carousel, data, OPTION)
 }
+
+export async function getNewAlbum() {
+	const data = Object.assign({}, PARAM, {
+		g_tk: 1278911659,
+		hostUin: 0,
+		platform: "yqq",
+		needNewCode: 0,
+		data: `{"albumlib":
+		{"method":"get_album_by_tags","param":
+		{"area":1,"company":-1,"genre":-1,"type":-1,"year":-1,"sort":2,"get_tags":1,"sin":0,"num":50,"click_albumid":0},
+		"module":"music.web_album_library"}}`
+	})
+	const option = {
+		param: "callback",
+		prefix: "callback"
+	}
+	return await jsonp(URL.newalbum, data, option)
+}
