18178269b4e401e47e591765fb812bf383d51573
weiwei.cai@zhangmen.com <weiwei.cai@zhangmen.com>
2018-11-07 16:14:56 +0800

    Scroll Component

---
+++ my-app/src/common/Scroll/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+import PropTypes from 'prop-types'
+import './index.css'
+import BScroll from 'better-scroll'
+
+class Scroll extends Component{
+  componentDidMount() {
+    this.scrollView = ReactDom.findDOMNode(this.refs.scrollView)
+    if (!this.bScroll) {
+      this.bScroll = new BScroll(this.scrollView, {
+        probeType: 3,
+        click: this.props.click
+      })
+    }
+    if (this.props.onScroll) this.bScroll.on('scroll', scroll => this.props.onScroll(scroll))
+  }
+
+  // 组件更新后，如果实例化了better-scroll并且需要刷新就调用refresh()函数
+  componentDidUpdate() {
+    if (this.bScroll && this.props.refresh) {
+      this.bScroll.refresh()
+    }
+  }
+
+  componentWillUnmount() {
+    this.bScroll.off('scroll')
+    this.bScroll.destroy()
+  }
+
+  render() {
+    return(
+      <div className="scroll-view" ref="scrollView">
+        {/**获取子组建**/}
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+// 对组件的props进行类型检查，这里使用prop-types库。类型检查是为了提早发现开发问题，避免一些bug产生
+Scroll.defaultProps = {
+  click: true,
+  refresh: false,
+  onScroll: null
+}
+
+Scroll.PropTypes = {
+  click: PropTypes.bool,
+  refresh: PropTypes.bool,
+  onScroll: PropTypes.func
+}
+
+export default Scroll
